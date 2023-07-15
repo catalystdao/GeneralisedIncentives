96c99b9f6c9ff1d6fa4a2d6119418a0b052efcd5
Alexander <alexander@lindgren.xyz>
2023-07-15 18:07:24 +0200

    reduce gas cost slightly

--- src/IncentivizedMessageEscrow.sol
+++ src/IncentivizedMessageEscrow.sol
@@ -268,13 +268,14 @@ abstract contract IncentivizedMessageEscrow is IIncentivizedMessageEscrow, Bytes
         uint256 deliveryFee; uint256 ackFee; uint256 sumFee; uint256 refund; uint256 gasSpentOnSource;
         unchecked {
             // gasSpentOnDestination * incentive.priceOfDeliveryGas < 2**48 * 2**96 = 2**144
-            deliveryFee = ((gasSpentOnDestination <= incentive.maxGasDelivery) ? gasSpentOnDestination : incentive.maxGasDelivery) * incentive.priceOfDeliveryGas;  // If more gas was spent then allocated, then only use the allocation.
+            if (incentive.maxGasDelivery <= gasSpentOnDestination) gasSpentOnDestination = incentive.maxGasDelivery;  // If more gas was spent then allocated, then only use the allocation.
+            deliveryFee = gasSpentOnDestination * incentive.priceOfDeliveryGas;  
             // Delay the gas limit computation until as late as possible. This should include the majority of gas spent.
             // gasLimit = gasleft() when less gas was spent, thus it is always larger than gasleft().
             gasSpentOnSource = gasLimit - gasleft();
-            if (incentive.maxGasAck <= gasSpentOnSource) gasSpentOnSource = incentive.maxGasAck;
+            if (incentive.maxGasAck <= gasSpentOnSource) gasSpentOnSource = incentive.maxGasAck;  // If more gas was spent then allocated, then only use the allocation.
             // gasSpentOnSource * incentive.priceOfAckGas < 2**48 * 2**96 = 2**144
-            ackFee = gasSpentOnSource * incentive.priceOfAckGas;  // If more gas was spent then allocated, then only use the allocation.
+            ackFee = gasSpentOnSource * incentive.priceOfAckGas;  
             // deliveryFee + ackFee < 2**144 + 2**144 = 2**145
             sumFee = deliveryFee + ackFee;
             // (incentive.priceOfDeliveryGas * incentive.maxGasDelivery + incentive.priceOfDeliveryGas * incentive.maxGasAck) has been caculated before (escrowBounty) < (2**48 * 2**96) + (2**48 * 2**96) = 2**144 + 2**144 = 2**145
