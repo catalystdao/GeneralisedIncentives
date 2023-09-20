be8b62ddf184bb878aab15606ade57cb3004b17a
Alexander <alexander@lindgren.xyz>
2023-09-20 12:53:13 +0200

    feat: add on-recv

--- src/IncentivizedMessageEscrow.sol
+++ src/IncentivizedMessageEscrow.sol
@@ -211,6 +211,8 @@ abstract contract IncentivizedMessageEscrow is IIncentivizedMessageEscrow, Bytes
      *  Please ensure that feeRecipitent can receive gas token: Either it is an EOA or a implement fallback() / receive().
      *  Likewise for any non-evm chains. Otherwise the message fails (ack) or the relay payment is lost (call).
      *  You need to pass in incentive.maxGas(Delivery|Ack) + messaging protocol dependent buffer, otherwise this call might fail.
+     * On Receive implementations make _verifyMessage revert. The result is
+     * that this endpoint is disabled.
      * @param messagingProtocolContext Additional context required to verify the message by the messaging protocol.
      * @param rawMessage The raw message as it was emitted.
      * @param feeRecipitent An identifier for the the fee recipitent. The identifier should identify the relayer on the source chain.
@@ -242,7 +244,7 @@ abstract contract IncentivizedMessageEscrow is IIncentivizedMessageEscrow, Bytes
     /**
      * @notice Handles call messages.
      */
-    function _handleCall(bytes32 sourceIdentifier, bytes memory sourceImplementationIdentifier, bytes calldata message, bytes32 feeRecipitent, uint256 gasLimit) internal {
+    function _handleCall(bytes32 sourceIdentifier, bytes memory sourceImplementationIdentifier, bytes calldata message, bytes32 feeRecipitent, uint256 gasLimit) internal returns(bytes memory ackMessageWithContext) {
         // Ensure message is unique and can only be execyted once
         bytes32 messageIdentifier = bytes32(message[MESSAGE_IDENTIFIER_START:MESSAGE_IDENTIFIER_END]);
 
@@ -294,7 +296,7 @@ abstract contract IncentivizedMessageEscrow is IIncentivizedMessageEscrow, Bytes
 
     
         // Encode a new message to send back. This lets the relayer claim their payment.
-        bytes memory ackMessageWithContext = abi.encodePacked(
+        ackMessageWithContext = abi.encodePacked(
             bytes1(DestinationtoSource),    // This is a sendMessage
             messageIdentifier,              // message identifier
             fromApplication,
@@ -314,7 +316,6 @@ abstract contract IncentivizedMessageEscrow is IIncentivizedMessageEscrow, Bytes
         // Send message to messaging protocol
         // The cost management is made by _sendMessage so we don't have to check if enough gas has been provided.
         _sendMessage(sourceIdentifier, sourceImplementationIdentifier, ackMessageWithContext);
-
     }
 
     /**
---
+++ src/apps/mock/onRecvMockEscrow.sol
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { IMETimeoutExtension } from "../../TimeoutExtension.sol";
+
+// This is an example contract which exposes an onReceive interface. This is for messaging protocols
+// where messages are delivered directly to the messaging protocol's contract rather than this contract.
+// Comments marked by * imply that an integration point should be changed by external contracts.
+contract OnRecvIncentivizedMockEscrow is IMETimeoutExtension {
+    error NotEnoughGasProvidedForVerification();
+    error NonVerifiableMessage();
+    bytes32 immutable public UNIQUE_SOURCE_IDENTIFIER;
+    address immutable public MESSAGING_PROTOCOL_CALLER;
+
+    struct VerifiedMessageHashContext {
+        bytes32 chainIdentifier;
+        bytes implementationIdentifier;
+    }
+
+    mapping(bytes32 => VerifiedMessageHashContext) public isVerifiedMessageHash;
+
+    event Message(
+        bytes32 destinationIdentifier,
+        bytes recipitent,
+        bytes message
+    );
+
+    constructor(address messagingProtocol) {
+        MESSAGING_PROTOCOL_CALLER = messagingProtocol;UNIQUE_SOURCE_IDENTIFIER = bytes32(uint256(111));
+    }
+
+    // Verify that the sender is correct.
+    modifier onlyMessagingProtocol() {
+        require(msg.sender == MESSAGING_PROTOCOL_CALLER);
+        _;
+    }
+
+    function estimateAdditionalCost() external pure returns(address asset, uint256 amount) {
+        asset =  address(0);
+        amount = 0;
+    }
+
+    function _getMessageIdentifier(
+        bytes32 destinationIdentifier,
+        bytes calldata message
+    ) internal override view returns(bytes32) {
+        return keccak256(
+            abi.encodePacked(
+                bytes32(block.number),
+                UNIQUE_SOURCE_IDENTIFIER, 
+                destinationIdentifier,
+                message
+            )
+        );
+    }
+
+    function _verifyMessage(bytes calldata _metadata, bytes calldata _message) internal view override returns (bytes32 sourceIdentifier, bytes memory implementationIdentifier, bytes calldata message_) {
+        sourceIdentifier = isVerifiedMessageHash[keccak256(_message)].chainIdentifier;
+        implementationIdentifier = isVerifiedMessageHash[keccak256(_message)].implementationIdentifier;
+        
+        if (sourceIdentifier == bytes32(0)) revert NonVerifiableMessage();
+
+        message_ = _message;
+    }
+
+    function onReceive(
+        bytes32 chainIdentifier,
+        bytes memory sourceImplementationIdentifier,
+        bytes calldata rawMessage,
+        bytes32 feeRecipitent
+    ) onlyMessagingProtocol external {
+        // _onReceive(chainIdentifier, rawMessage, feeRecipitent);
+        uint256 gasLimit = gasleft();
+        bytes memory ackMessage = _handleCall(chainIdentifier, sourceImplementationIdentifier, rawMessage, feeRecipitent, gasLimit);
+
+        // Send ack:
+        _sendMessage(chainIdentifier, sourceImplementationIdentifier, ackMessage);
+        // * For an actual implementation, the _sendMessage might also be implemented as a return value for onReceive like:
+        // * return ReturnStruct?({chainIdentifier: chainIdentifier, message: ackMessage});
+    }
+
+    // The escrow manages acks, so any message can be directly provided to _onReceive.
+    function onAck(
+        bytes32 chainIdentifier,
+        bytes memory destinationImplementationIdentifier,
+        bytes calldata rawMessage,
+        bytes32 feeRecipitent
+    ) onlyMessagingProtocol external {
+        uint256 gasLimit = gasleft();
+        isVerifiedMessageHash[keccak256(abi.encodePacked(
+            chainIdentifier, rawMessage
+        ))] = VerifiedMessageHashContext({
+            chainIdentifier: chainIdentifier,
+            implementationIdentifier: destinationImplementationIdentifier
+        });
+        _handleAck(chainIdentifier, destinationImplementationIdentifier, rawMessage, feeRecipitent, gasLimit);
+    }
+
+    // For timeouts, we need to construct the message.
+    function onTimeout(
+        bytes32 chainIdentifier,
+        bytes memory /* destinationImplementationIdentifier */,
+        bytes calldata rawMessage,
+        bytes32 feeRecipitent
+    ) onlyMessagingProtocol external {
+        uint256 gasLimit = gasleft();
+        _handleTimeout(chainIdentifier, rawMessage, feeRecipitent, gasLimit);
+    }
+
+    // * Send to messaging_protocol 
+    function _sendMessage(bytes32 destinationChainIdentifier, bytes memory destinationImplementation, bytes memory message) internal override returns(uint128 costOfSendMessageInNativeToken) {
+        emit Message(
+            destinationChainIdentifier,
+            destinationImplementation,
+            abi.encodePacked(
+                UNIQUE_SOURCE_IDENTIFIER,
+                destinationChainIdentifier,
+                message
+            )
+        );
+        return 0;
+    }
+}
