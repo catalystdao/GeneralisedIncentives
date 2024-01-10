f9bb505ab656814e04f808d239ae6b52cd630d0b
Alexander <alexander@lindgren.xyz>
2024-01-10 09:03:10 +0100

    feat: refund gas to should never be set to address(0)

---
+++ test/IncentivizedMessageEscrow/escrowMessage/refundGasTo.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import { TestCommon } from "../../TestCommon.t.sol";
+
+contract EscrowInformationTest is TestCommon {
+    function test_error_refund_gas_to_0() public {
+        IncentiveDescription storage incentive = _INCENTIVE;
+        incentive.refundGasTo = address(0);
+        vm.expectRevert();
+        (, bytes32 messageIdentifier) = escrow.submitMessage{value: _getTotalIncentive(_INCENTIVE)}(
+            bytes32(uint256(0x123123) + uint256(2**255)),
+            _DESTINATION_ADDRESS_THIS,
+            _MESSAGE,
+            incentive
+        );
+    }
+
+}
