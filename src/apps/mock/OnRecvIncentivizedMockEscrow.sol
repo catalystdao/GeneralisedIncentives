b4e27b2ef6e6c2754b64016517d470a0fb1b8276
Alexander <git@lindgren.xyz>
2024-04-30 15:37:50 +0200

    fix: set solidity 0.8.22 as minimum

--- src/apps/mock/OnRecvIncentivizedMockEscrow.sol
+++ src/apps/mock/OnRecvIncentivizedMockEscrow.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.22;
 
 import { IncentivizedMessageEscrow } from "../../IncentivizedMessageEscrow.sol";
 import { MockOnRecvAMB } from "../../../test/mocks/MockOnRecvAMB.sol";
