c490e149b1c0116048f41cecf27af4503f98abb2
Alexander <git@lindgren.xyz>
2024-05-01 15:28:25 +0200

    test: fix last 2 tests

--- test/IncentivizedMessageEscrow/processMessage/Reentry.call.t.sol
+++ test/IncentivizedMessageEscrow/processMessage/Reentry.call.t.sol
@@ -61,7 +61,7 @@ contract CallReentryTest is TestCommon, ICrossChainReceiver {
                 messageIdentifier,
                 _DESTINATION_ADDRESS_APPLICATION,
                 feeRecipient,
-                uint48(0xfc9b),  // Gas used
+                uint48(0xfcb9),  // Gas used
                 uint64(1),
                 uint8(1)
             )
