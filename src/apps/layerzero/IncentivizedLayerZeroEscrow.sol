185c27719b0de52754e0c4bc1405a413efe0d013
Alexander <alexander@lindgren.xyz>
2024-05-27 15:10:26 +0200

    feat: send in endpoint v2

--- src/apps/layerzero/IncentivizedLayerZeroEscrow.sol
+++ src/apps/layerzero/IncentivizedLayerZeroEscrow.sol
@@ -2,26 +2,24 @@
 pragma solidity ^0.8.13;
 
 import { IncentivizedMessageEscrow } from "../../IncentivizedMessageEscrow.sol";
-import { ILayerZeroValidationLibrary } from "./interfaces/ILayerZeroValidationLibrary.sol";
-import { ILayerZeroValidationLibrary } from "./interfaces/ILayerZeroValidationLibrary.sol";
-import { ILayerZeroEndpoint } from "./interfaces/ILayerZeroEndpoint.sol";
+import { ILayerZeroEndpointV2, MessagingParams } from "./interfaces/ILayerZeroEndpointV2.sol";
 
 /**
  * @notice LayerZero escrow.
  * Do not use because of license issues.
  */
-abstract contract BareIncentivizedLayerZeroEscrow is IncentivizedMessageEscrow, ILayerZeroValidationLibrary {
+abstract contract BareIncentivizedLayerZeroEscrow is IncentivizedMessageEscrow {
     error LayerZeroCannotBeAddress0();
 
-    ILayerZeroEndpoint immutable LAYER_ZERO;
+    ILayerZeroEndpointV2 immutable LAYER_ZERO;
 
     // chainid is immutable on LayerZero endpoint, so we read it and store it likewise.
-    uint16 public immutable chainId;
+    uint32 public immutable chainId;
 
     constructor(address sendLostGasTo, address layer_zero) IncentivizedMessageEscrow(sendLostGasTo) {
         if (layer_zero == address(0)) revert LayerZeroCannotBeAddress0();
-        LAYER_ZERO = ILayerZeroEndpoint(layer_zero);
-        chainId  = LAYER_ZERO.getChainId();
+        LAYER_ZERO = ILayerZeroEndpointV2(layer_zero);
+        chainId  = LAYER_ZERO.eid();
     }
 
     function estimateAdditionalCost() external view returns(address asset, uint256 amount) {
@@ -65,24 +63,21 @@ abstract contract BareIncentivizedLayerZeroEscrow is IncentivizedMessageEscrow, 
     }
 
     function _sendPacket(bytes32 destinationChainIdentifier, bytes memory destinationImplementation, bytes memory message) internal override returns(uint128 costOfsendPacketInNativeToken) {
-        // Handoff package to LZ.
-        uint16 _dstChainId = uint16(uint256(destinationChainIdentifier));
-        // From LZ docs: "the address on destination chain (in bytes). address length/format may vary by chains"
-        // bytes memory _destination = destinationImplementation;
-        // bytes memory _payload = message;
-        address payable _refundAddress = payable(0); // TODO: What here?
-        address _zroPaymentAddress = address(0); // TODO: What here?
-        bytes memory _adapterParams = hex"";
 
-        LAYER_ZERO.send(
-            _dstChainId,
-            destinationImplementation,
-            message,
-            _refundAddress,
-            _zroPaymentAddress,
-            _adapterParams
+        costOfsendPacketInNativeToken = 0; // TODO
+
+        // Handoff package to LZ.
+        LAYER_ZERO.send{value: costOfsendPacketInNativeToken}(
+            MessagingParams({
+                dstEid: uint32(uint256(destinationChainIdentifier)),
+                receiver: bytes32(destinationImplementation),
+                message: message,
+                options: hex"",
+                payInLzToken: false
+            }),
+            msg.sender // TODO:
         );
 
-        return costOfsendPacketInNativeToken = uint128(0);
+        return costOfsendPacketInNativeToken;
     }
 }
--- src/apps/layerzero/interfaces/ILayerZeroValidationLibrary.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: BUSL-1.1
-
-pragma solidity >=0.8.0;
-
-import "../utility/LayerZeroPacket.sol";
-
-interface ILayerZeroValidationLibrary {
-    function validateProof(bytes32 blockData, bytes calldata _data, uint _remoteAddressSize) external returns (LayerZeroPacket.Packet memory packet);
-}
