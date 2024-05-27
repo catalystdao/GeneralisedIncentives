51676ddc9b80c14dd06890f656f40ed7edf46e75
Alexander <alexander@lindgren.xyz>
2024-05-27 15:10:26 +0200

    test: prepare tests

---
+++ test/layerzero/mock/MockLayerZeroEscrow.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: IncentivizedLayerZeroEscrow
+pragma solidity ^0.8.13;
+
+import { IncentivizedLayerZeroEscrow } from "../../../src/apps/layerzero/IncentivizedLayerZeroEscrow.sol";
+
+/**
+ * @notice Mock Layer Zero Escrow
+ */
+contract MockLayerZeroEscrow is IncentivizedLayerZeroEscrow {
+
+    constructor(address sendLostGasTo, address lzEndpointV2, address ULN) IncentivizedLayerZeroEscrow(sendLostGasTo, lzEndpointV2, ULN) {}
+
+    function verifyPacket(bytes calldata _packetHeader, bytes calldata _packet) external view returns(bytes32 sourceIdentifier, bytes memory implementationIdentifier, bytes calldata message_) {
+        return _verifyPacket(_packetHeader, _packet);
+    }
+
+    function sendPacket(bytes32 destinationChainIdentifier, bytes memory destinationImplementation, bytes memory message) external returns(uint128 costOfsendPacketInNativeToken) {
+        return _sendPacket(destinationChainIdentifier, destinationImplementation, message);
+    }
+}
